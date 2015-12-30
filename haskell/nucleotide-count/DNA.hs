6f4495541157ce8a228da1b0b4b4fcf3fc2b6298
Matthias Iacsa <matthias.iacsa@gmail.com>
2015-12-30 01:03:51 +0100

    fix (nucleotide-count): Adapt to new test suite
    The new test suite requires `nucleotideCounts` to throw an error, which
    previous versions did not.
    
    Extract the error raising from `count` into its own method, and include
    this new method `validate` into both `count` and `nucleotideCounts`.

--- haskell/nucleotide-count/DNA.hs
+++ haskell/nucleotide-count/DNA.hs
@@ -4,12 +4,16 @@ import Data.Map (Map, fromListWith, findWithDefault)
 
 type Nucleotide = Char
 
+validate :: Nucleotide -> Nucleotide
+validate c
+  | elem c "ATCG" = c
+  | otherwise = error $ "invalid nucleotide '" ++ [c] ++ "'"
+
 count :: Nucleotide -> [Nucleotide] -> Integer
-count c ss = findWithDefault err c (nucleotideCounts ss)
-  where err = error $ "invalid nucleotide '" ++ [c] ++ "'"
+count c ss = findWithDefault 0 (validate c) (nucleotideCounts ss)
 
 nucleotideCounts :: [Nucleotide] -> Map Nucleotide Integer
 nucleotideCounts ss = fromListWith (+) (defaults ++ nucleos)
   where
     defaults = map (,0) "ATCG"
-    nucleos  = map (,1) ss
+    nucleos  = map ((,1) . validate) ss
