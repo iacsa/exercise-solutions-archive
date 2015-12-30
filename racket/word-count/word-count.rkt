9700ad9130612cdce91897fa16b4b42ecbc9a78c
Matthias Iacsa <matthias.iacsa@gmail.com>
2015-12-30 03:20:47 +0100

    refactor (racket/word-count): Remove old comments
    The file contains commented out code from a previous iteration. This
    code was initially kept for easy roll-back, but is no longer necessary.
    
    Delete old commented out code.

--- racket/word-count/word-count.rkt
+++ racket/word-count/word-count.rkt
@@ -2,19 +2,6 @@
 
 (provide word-count)
 
-;(define (word-count text)
-;  (let ([words (string-split text)])
-;    (make-hash
-;      (remove-duplicates
-;        (map (λ (w) (cons w (count (curry string=? w) words))) words)))))
-
-
-;(define (mutable-to-immutable-hash table)
-;  (apply hash
-;    (flatten
-;      (for/list ([(k v) (in-hash table)])
-;        (list k v)))))
-
 (define (word-count text)
   (for/fold ([acc (hash)])
             ([word (in-list (string-split text))])
