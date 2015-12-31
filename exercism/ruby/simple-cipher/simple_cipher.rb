580185aec261430d6e475d3ff55f564264089c5d
Matthias Iacsa <matthias.iacsa@gmail.com>
2015-12-31 23:11:08 +0100

    refactor: Extract and deduplicate logic

--- exercism/ruby/simple-cipher/simple_cipher.rb
+++ exercism/ruby/simple-cipher/simple_cipher.rb
@@ -1,18 +1,38 @@
 class Cipher
   VERSION = 1
+
   attr_reader :key
-  def initialize(key = nil)
-    raise ArgumentError if key and not /^[a-z]+$/ =~ key 
-    @key = key || Array.new(100){|i| rand(26) + "a".ord}.map(&:chr).join
+  @@char_range = ("a" .. "z").to_a
+
+  def initialize(key = @@char_range.sample(100).join)
+    raise ArgumentError unless (/^[a-z]+$/ =~ key)
+    @key = key
+    @cycle_key = key.each_char.cycle
   end
+
   def encode(text)
-    text.chars.zip(@key.chars.cycle).map do |v, k|
-      ((v.ord + k.ord - 2 * "a".ord) % 26 + "a".ord).chr
+    shift(text, 1)
+  end
+
+  def decode(text)
+    shift(text, -1)
+  end
+
+private
+
+  def shift(text, direction)
+    with_key(text).map do |char, key|
+      shift_char(char, direction * key)
     end.join
   end
-  def decode(text)
-    text.chars.zip(@key.chars.cycle).map do |v, k|
-      ((v.ord - k.ord) % 26 + "a".ord).chr
-    end.join
+
+  def shift_char(char, delta)
+    @@char_range[(char + delta) % 26]
+  end
+
+  def with_key(text)
+    text.each_char.zip(@cycle_key).map do |v, k|
+      [@@char_range.index(v), @@char_range.index(k)]
+    end
   end
 end
