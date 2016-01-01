2f5df2c6f659012316916bcc2167639cf1a38072
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-01 02:20:33 +0100

    refactor: Increase readability, remove superfluous calls

--- exercism/ruby/pythagorean-triplet/pythagorean_triplet.rb
+++ exercism/ruby/pythagorean-triplet/pythagorean_triplet.rb
@@ -1,40 +1,52 @@
 require "rsa"
 
 class Triplet
-  def initialize (a, b, c)
+
+  def initialize(a, b, c)
     @a, @b, @c = a, b, c
   end
+
   def sum
     @a + @b + @c
   end
+
   def product
     @a * @b * @c
   end
+
   def pythagorean?
-    @a**2 + @b**2 == @c**2
+    @a ** 2 + @b ** 2 == @c ** 2
   end
   
-  def self.where (opts)
-    min = (opts[:min_factor] ||  1).to_f
-    max = (opts[:max_factor] || 10).to_f
-    sum = opts[:sum]
+  # Using Euclid's formula
+  def self.where(min_factor: 1, max_factor: 10, sum: nil)
+    one_plus_sqrt_two = 1 + Math.sqrt(2)
 
-    (1..max).each_with_object([]) do |k, triples|
-      mmin = Math.sqrt(min / k).ceil  # a >= min
-      mmax = Math.sqrt(max / k).floor # b <= max
+    (1 .. max_factor).each_with_object([]) do |k, triples|
 
-      (mmin..mmax).each do |m|
-        nmin = (m / (1 + Math.sqrt(2))).ceil  # b >= a
-        nmax = [
-          Math.sqrt(m**2 - min / k), # a >= min
-          Math.sqrt(max / k - m**2)  # c <= max
-        ].min.floor
+      max_over_k = max_factor.to_f / k
+      min_over_k = min_factor.to_f / k
 
-        (nmin..nmax).select{|n| RSA::Math.coprime?(m, n)}.each do |n|
-          a = k*(m**2 - n**2)
-          b = k*(2*m*n)
-          c = k*(m**2 + n**2)
-          triples.push(Triplet.new(a, b, c)) if !sum || a + b + c == sum
+      m_min = Math.sqrt(min_over_k).ceil # a >= min_factor
+      m_max = Math.sqrt(max_over_k) # b <= max_factor
+
+      m_min.upto(m_max) do |m|
+        m2 = m * m
+
+        n_min = (m / one_plus_sqrt_two).ceil # b >= a
+        n_max = [
+          Math.sqrt(m2 - min_over_k), # a >= min_factor
+          Math.sqrt(max_over_k - m2) # c <= max_factor
+        ].min
+
+        n_min.upto(n_max) do |n|
+          next unless RSA::Math.coprime?(m, n)
+          n2 = n * n
+
+          a = k * (m2 - n2)
+          b = k * (2 * m * n)
+          c = k * (m2 + n2)
+          triples << Triplet.new(a, b, c) if sum.nil? || a + b + c == sum
         end
       end
     end
