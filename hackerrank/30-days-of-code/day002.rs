8e4046b8bb28617d8f1da47013b693e4fed0308e
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-01 16:47:51 +0100

    feat: New day, new 30 days of code solution

---
+++ hackerrank/30-days-of-code/day002.rs
@@ -0,0 +1,11 @@
+fn main() {
+  println!("Primitive : Double");
+  println!("Primitive : Character");
+  println!("Primitive : Boolean");
+  println!("Primitive : Integer");
+  println!("Referenced : String");
+  println!("Primitive : Boolean");
+  println!("Primitive : Double");
+  println!("Primitive : Character");
+  println!("Referenced : String");
+}
