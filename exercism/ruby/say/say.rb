153ff97cf0f875c26b9fbf88f5e7ef70ca3aa19e
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-03 12:08:25 +0100

    refactor: Adapt better coding style

--- exercism/ruby/say/say.rb
+++ exercism/ruby/say/say.rb
@@ -2,26 +2,29 @@ class Say
   @@ones = [nil, "one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine",
             "ten", "eleven", "twelve", "thirteen", "fourteen",
-            "fifteen", "sixteen", "seventeen", "eightteen", "nineteen"]
+            "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
   @@tens = [nil, nil, "twenty", "thirty", "forty", "fifty",
             "sixty", "seventy", "eighty", "ninety"]
   @@exps = [nil, nil, "hundred", "thousand", nil, nil, "million",
             nil, nil, "billion", nil, nil, "trillion"]
-  def initialize (str)
+
+  def initialize(str)
     @val = str.to_i
-    raise ArgumentError if @val < 0 or 999999999999 < @val
+    raise ArgumentError if @val < 0 || 10**12 <= @val
   end
+
   def in_english
-    if @val == 0
-      "zero"
-    else
-      [9, 6, 3, 0].map{|i|
-        num = hundreds((@val / 10**i) % 10**3)
-        num.size > 0 ? "#{num}#{i>0?" ":""}#{@@exps[i]}" : nil
-      }.reject(&:nil?).join(" ")
-    end
+    return "zero" if @val == 0
+
+    12.step(0, -3).map do |i|
+      num = hundreds((@val / 10**i) % 10**3)
+      num.size > 0 ? "#{num}#{i > 0 ? " " : ""}#{@@exps[i]}" : nil
+    end.reject(&:nil?).join(" ")
   end
-  def hundreds (n)
+
+private
+
+  def hundreds(n)
     h, t, o = n / 100, (n % 100) / 10, n % 10
     res = (h >= 1 ? ["#{@@ones[h]} hundred"] : [])
     if t < 2
