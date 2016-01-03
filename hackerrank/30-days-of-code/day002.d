540dde0018628f16d9f98d640ff5b4d3f72f3b20
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-03 14:56:20 +0100

    feat: Remove unnecessary parentheses

--- hackerrank/30-days-of-code/day002.d
+++ hackerrank/30-days-of-code/day002.d
@@ -5,13 +5,13 @@ import std.string: strip;
 
 void main(string[] args) {
   // Doesn't hurt to read all as reals
-  auto original_price = readln().strip().to!real();
-  auto tip_ratio = readln().strip().to!real() / 100;
-  auto tax_ratio = readln().strip().to!real() / 100;
+  auto original_price = readln.strip.to!real;
+  auto tip_ratio = readln.strip.to!real / 100;
+  auto tax_ratio = readln.strip.to!real / 100;
 
   auto full_price = original_price * (1 + tip_ratio + tax_ratio);
   
-  auto rounded_price = full_price.lround().to!int();
+  auto rounded_price = full_price.lround.to!int;
   
   writefln("The final price of the meal is $%d.", rounded_price);
 }
