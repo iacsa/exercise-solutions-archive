5631da9b4404643bd49e47c2a7cbffdcb626bcbb
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-03 19:51:31 +0100

    refactor: Improve style

--- exercism/ruby/leap/leap.rb
+++ exercism/ruby/leap/leap.rb
@@ -1,6 +1,7 @@
 class Year
   VERSION = 1
-  def Year.leap? (n)
-    (n % 4).zero? and not (n % 100).zero? or (n % 400).zero?
+
+  def Year.leap?(n)
+     (n % 400).zero? || (n % 4).zero? && (n % 100).positive?
   end
 end
