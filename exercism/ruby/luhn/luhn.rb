aef2e12af23d1b34f53046c64a6223e5c04d0a33
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 22:46:29 +0100

    refactor: Get rid of numerical loop
    Numeric operations in Ruby are not very fast. From a point of
    readability it's better to go with high-level alternatives.

--- exercism/ruby/luhn/luhn.rb
+++ exercism/ruby/luhn/luhn.rb
@@ -1,24 +1,28 @@
 class Luhn
-  def Luhn.create (n)
-    (0..9).each do |i|
-      return n * 10 + i if Luhn.new(n * 10 + i).valid?
+  attr_reader :addends
+
+  def self.create(n)
+    n * 10 + (-Luhn.new(n * 10).checksum % 10)
+  end
+
+  def initialize(n)
+    @addends = []
+
+    str = n.to_s
+    str.size.downto(1) do |i|
+      # Use negative indexing so that we can iterate forward,
+      # and still know whether we are even or odd from the end
+      x = str[-i].to_i
+      x *= 2 if i.even?
+      x -= 9 if x > 9
+      @addends << x 
     end
   end
-  def initialize (n)
-    @n = n
-  end
-  def addends
-    n, res, double = @n, [], false
-    while n > 0
-      k, n = n % 10, n / 10
-      res << -> x {x > 9 ? x - 9 : x}[double ? 2 * k : k]
-      double = !double
-    end
-    res.reverse
-  end
+
   def checksum
     addends.reduce(:+)
   end
+
   def valid?
     (checksum % 10).zero?
   end
