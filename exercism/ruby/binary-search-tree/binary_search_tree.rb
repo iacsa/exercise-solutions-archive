bf72f479a094cc4a4ebe4e67c421ebc539d8f756
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 20:43:14 +0100

    fix: Correct tests for `nil`

--- exercism/ruby/binary-search-tree/binary_search_tree.rb
+++ exercism/ruby/binary-search-tree/binary_search_tree.rb
@@ -1,26 +1,31 @@
 class Bst
+  include Enumerable
+
   attr_reader :data, :left, :right
-  def initialize (val)
+
+  def initialize(val)
     @data = val
   end
-  def insert (val)
+
+  def insert(val)
     if val <= @data
-      if @left
-        @left.insert(val)
-      else
+      if @left.nil?
         @left = Bst.new(val)
+      else
+        @left.insert(val)
       end
     else
-      if @right
-        @right.insert(val)
-      else
+      if @right.nil?
         @right = Bst.new(val)
+      else
+        @right.insert(val)
       end
     end
   end
+
   def each 
-    @left.each{|i| yield(i)} if @left
+    @left.each{|i| yield(i) } unless @left.nil?
     yield(@data)
-    @right.each{|i| yield(i)} if @right
+    @right.each{|i| yield(i) } unless @right.nil?
   end
 end
