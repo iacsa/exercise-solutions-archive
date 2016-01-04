fec3623a0a55b39192e4bbb01e1aa7a94d36b050
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 21:38:02 +0100

    refactor: Simplify validation of input list

--- exercism/ruby/binary-search/binary_search.rb
+++ exercism/ruby/binary-search/binary_search.rb
@@ -1,8 +1,8 @@
 class BinarySearch
   attr_reader :list
 
-  def initialize (list)
-    raise ArgumentError unless list.each_cons(2).map{|s| s[1]-s[0] >= 0}.all?
+  def initialize(list)
+    raise ArgumentError unless list.each_cons(2).all?{|i, j| i <= j }
     @list = list
   end
 
@@ -10,10 +10,11 @@ class BinarySearch
     @list.size / 2
   end
 
-  def search_for (value, low = 0, high = @list.size - 1)
+  def search_for(value, low = 0, high = @list.size - 1)
     raise RuntimeError if low > high
 
     pivot = (high + low) / 2
+
     case value <=> @list[pivot]
     when -1 then search_for(value, low, pivot - 1)
     when  0 then pivot
