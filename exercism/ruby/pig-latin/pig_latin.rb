b4b4c872e27885bd90f0a2e034a5795dbdaf6b47
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 22:58:25 +0100

    refactor: Simplify word translation

--- exercism/ruby/pig-latin/pig_latin.rb
+++ exercism/ruby/pig-latin/pig_latin.rb
@@ -1,16 +1,18 @@
 class PigLatin
-  def PigLatin.translate str
-    str.split.map{|s| PigLatin.translate_one(s)}.join(" ")
+
+  def self.translate(text)
+    text.split.map{|word| translate_word(word) }.join(" ")
   end
-  def PigLatin.translate_one str
-    if /^([bcdfghlklmnpqrstvwxyz]qu|thr|sch)/ =~ str
-      str[3...str.size] + str[0..2] + "ay"
-    elsif /^(ch|qu|th)/ =~ str
-      str[2...str.size] + str[0..1] + "ay"
-    elsif /^(ye|[bcdfghlklmnpqrstvwz]|[xy][aeiou])/ =~ str
-      str[1...str.size] + str[0] + "ay"
-    else
-      str + "ay"
+
+private
+
+  def self.translate_word(word)
+    index = case
+    when /^([bcdfghlklmnpqrstvwxyz]qu|thr|sch)/ =~ word then 3
+    when /^(ch|qu|th)/ =~ word then 2
+    when /^(ye|[bcdfghlklmnpqrstvwz]|[xy][aeiou])/ =~ word then 1
+    else 0
     end
+    word.chars.rotate(index).join + "ay"
   end
 end
