9c34da2ab320ab5769c52387b5c746d2f068b150
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 23:08:53 +0100

    refactor: Refactor a lot :-D

--- exercism/ruby/proverb/proverb.rb
+++ exercism/ruby/proverb/proverb.rb
@@ -1,12 +1,12 @@
 class Proverb
-  def initialize (*ss)
-    dict = ss.last.is_a?(Hash) ? ss.pop : {}
-    @q = dict[:qualifier]
+  def initialize(*ss, qualifier: nil)
+    @q = qualifier&. + " "
     @ss = ss
   end
+
   def to_s
-    (@ss.each_cons(2).map{|a, b|
-    "For want of a #{a} the #{b} was lost."
-    } + ["And all for the want of a #{@q ? @q + " " : ""}#{@ss.first}."]).join("\n")
+    @ss.each_cons(2).map do |a, b|
+      "For want of a #{a} the #{b} was lost.\n"
+    end.join + "And all for the want of a #{@q}#{@ss.first}."
   end
 end
