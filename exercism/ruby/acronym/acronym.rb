83b3389005213a7e581f9aa420234f56b42f3efc
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-04 22:05:11 +0100

    style: Fix typo in comments

--- exercism/ruby/acronym/acronym.rb
+++ exercism/ruby/acronym/acronym.rb
@@ -1,10 +1,10 @@
 class Acronym
   VERSION = 1
 
-  def self.abbreviate (phrase)
+  def self.abbreviate(phrase)
     # We want to include in the abbreviation:
     # 1. Any capital letter not immediately preceded by another capital letter
-    # 2. Any lowercase letter not immediately preceded by any another letter
+    # 2. Any lowercase letter not immediately preceded by any other letter
     phrase.scan(/((?<![A-Z])[A-Z]|(?<![a-zA-Z])[a-z])/).join.upcase
   end
 end
