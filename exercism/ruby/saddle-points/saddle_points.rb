4a32139f5394025fab4ff674c28a3da24a08f64b
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-05 00:47:30 +0100

    refactor: Cleanup old mistakes

--- exercism/ruby/saddle-points/saddle_points.rb
+++ exercism/ruby/saddle-points/saddle_points.rb
@@ -1,23 +1,28 @@
 class Matrix
+
   def initialize (str)
-    lines = str.split("\n")
-    @n = lines.size
-    @mat = Array.new(@n){Array.new}
-    lines.each_with_index{|l, i| l.split.each{|k| @mat[i] << k.to_i}}
-    @m = @mat[0].size
+    @mat = str.split("\n").map do |row|
+      row.split.map(&:to_i)
+    end
+    @m, @n = @mat.size, @mat.first.size
   end
+
   def rows
-    -> i {@mat[i]}
+    @mat
   end
+
   def columns
-    -> j {@mat.collect{|row| row[j]}}
+    @mat.transpose
   end
+
   def saddle_points
-    (0...@n).to_a.product((0...@n).to_a).reduce([]){|a, v|
-      saddle?(v[0], v[1]) ? (a + [v]) : a
-    }
+    (0 ... @m).to_a.product((0 ... @n).to_a).select do |i, j|
+      saddle?(i, j)
+    end
   end
-  def saddle? (i, j)
-    @mat[i][j] == rows[i].max and @mat[i][j] == columns[j].min
+
+  def saddle?(i, j)
+    v = @mat[i][j]
+    v == rows[i].max && v == columns[j].min
   end
 end
