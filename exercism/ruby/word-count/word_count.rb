c7447746269391fb06ca649947e4ea05664ab312
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-05 01:06:58 +0100

    refactor: Break up chained calls

--- exercism/ruby/word-count/word_count.rb
+++ exercism/ruby/word-count/word_count.rb
@@ -1,11 +1,14 @@
 class Phrase
   VERSION = 1
+
   attr_reader :word_count
-  def initialize (line)
-    @word_count =
-      line.downcase
-          .split(/'?[^[:alnum:]']'?/) # Differentiate singe quotes and apostrophes
-          .select{|w| w.size > 0 }
-          .inject({}){|d, w| d[w] = (d[w] || 0) + 1; d }
+
+  def initialize(line)
+    @word_count = Hash.new{|h, k| h[k] = 0 }
+
+    # Differentiate single quotes and apostrophes
+    line.downcase.split(/'?[^[:alnum:]']'?/).each do |word|
+      @word_count[word] += 1 unless word.empty?
+    end
   end
 end
