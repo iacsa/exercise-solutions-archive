9459ea7c72fd3f0b69fcd480ee738ba3f4fb343e
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-05 12:16:21 +0100

    refactor: add contracts, use lists instead of streams

--- exercism/racket/perfect-numbers/perfect-numbers.rkt
+++ exercism/racket/perfect-numbers/perfect-numbers.rkt
@@ -1,15 +1,16 @@
 #lang racket
 
-(provide perfect-numbers)
+(require (only-in math/number-theory
+                  divisor-sum))
 
-(require math/number-theory)
+
+(provide
+  (contract-out 
+    [perfect-numbers (-> natural-number/c (listof natural-number/c))]))
+
 
 (define (perfect-numbers n)
-  (stream->list
-    (stream-filter 
-      (λ (n) (= n (sum-of-divisors n)))
-      (in-range 2 (add1 n)))))
-
-; The divisors procedure includes n itself, so we need to subtract it
-(define (sum-of-divisors n)
-  (- (apply + (divisors n)) n))
+  (filter 
+    ; divisor-sum also counts `n` itself
+    (λ (n) (= (* 2 n) (divisor-sum n)))
+    (range 2 (add1 n))))
