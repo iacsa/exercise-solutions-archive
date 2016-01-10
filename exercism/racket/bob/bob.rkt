9a0439908ad44b39d7004fae4552350763c7087f
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 18:06:56 +0100

    refactor: Add contract, extract function

--- exercism/racket/bob/bob.rkt
+++ exercism/racket/bob/bob.rkt
@@ -1,17 +1,23 @@
 #lang racket
 
-(provide response-for)
+(provide
+  (contract-out
+    [response-for (-> string? string?)]))
+
 
 (define (response-for query)
   (cond
     [(shouting? query) "Whoa, chill out!"]
-    [(string-suffix? query "?") "Sure."]
+    [(asking? query) "Sure."]
     [(silence? query) "Fine. Be that way!"]
-    [#t "Whatever."]))
+    [else "Whatever."]))
 
 (define (shouting? s)
   (and (string=? s (string-upcase s))
        (not (string=? s (string-downcase s)))))
+
+(define (asking? s)
+  (string-suffix? s "?"))
 
 (define (silence? s)
   (not (non-empty-string? (string-trim s))))
