db9ea6f8d431c5185cde471756612d42546c1697
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 19:43:45 +0100

    refactor: Use `for/list` instead of `for/fold`
    Instead of manually building a list with `for/fold` we could do the same
    thing more simply with `for/list`.

--- exercism/racket/grep/grep.rkt
+++ exercism/racket/grep/grep.rkt
@@ -2,29 +2,26 @@
 
 (provide
   (contract-out
-    [grep-file (-> path-string? regexp? (listof (cons/c natural-number/c string?)))]))
-  ;; Find lines in a file matching a regular expression pattern.
-  ;; Output is a list of pairs containing the number and contents of
-  ;;  each matching line.
+    [grep-file (-> path-string?
+                   regexp?
+                   (listof (cons/c natural-number/c string?)))]))
 
-;; =============================================================================
 
 (module+ main
   (match (current-command-line-arguments)
     [(vector pattern files ...)
-     (let ([regex (regexp pattern)])
-       (for ([file (in-list files)])
-         (print-grep-results file regex)))]
+     (define regex (regexp pattern))
+     (for ([file (in-list files)])
+       (print-grep-results file regex))]
     [_ (error "grep.rkt: Use \"racket grep.rkt PATTERN FILE ...\"")]))
 
+
 (define (grep-file file regex)
-  (reverse
-    (for/fold ([acc '()])
-              ([nr (in-naturals 1)]
-               [line (in-list (file->lines file))]
-               #:when (regexp-match? regex line))
-      (cons (cons nr line) acc))))
+  (for/list ([nr (in-naturals 1)]
+             [line (in-list (file->lines file))]
+             #:when (regexp-match? regex line))
+    (cons nr line)))
 
 (define (print-grep-results file regex)
-  (for ([line (in-list (grep-file file regex))])
-    (printf "~a ~a: ~a~n" file (car line) (cdr line))))
+  (for ([grep-match (in-list (grep-file file regex))])
+    (printf "~a ~a: ~a~n" file (car grep-match) (cdr grep-match))))
