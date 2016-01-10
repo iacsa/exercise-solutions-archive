5c20a6e3f7f3a516b31991d8dffb2aed41c8d45e
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 18:25:55 +0100

    refactor: Add contract

--- exercism/racket/rna-transcription/rna-transcription.rkt
+++ exercism/racket/rna-transcription/rna-transcription.rkt
@@ -1,6 +1,20 @@
 #lang racket
 
-(provide to-rna)
+(provide
+  (contract-out
+    [to-rna (-> dna? rna?)]))
+
+
+(define (dna? strand)
+  (string-of? strand '(#\A #\C #\G #\T)))
+
+(define (rna? strand)
+  (string-of? strand '(#\A #\C #\G #\U)))
+
+(define (string-of? string allowed-chars)
+  (and (string? string)
+       (for/and ([char (in-string string)]) (member char allowed-chars))))
+
 
 (define (to-rna dna)
   (list->string
@@ -9,5 +23,4 @@
         [#\A #\U]
         [#\C #\G]
         [#\G #\C]
-        [#\T #\A]
-        [_ (error "to-rna: Invalid character in dna-string.")]))))
+        [#\T #\A]))))
