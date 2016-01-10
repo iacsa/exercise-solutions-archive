a21f11b6c6a6dc0f95bd2204e0dc7d2b92fc2304
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 20:51:28 +0100

    refactor: Replace recursion with loop

--- exercism/racket/roman-numerals/roman-numerals.rkt
+++ exercism/racket/roman-numerals/roman-numerals.rkt
@@ -4,19 +4,19 @@
   (contract-out
     [to-roman (-> natural-number/c string?)]))
 
-(define roman-values
+
+(define roman-numerals
   '(("M" . 1000) ("CM" . 900) ("D" . 500)
     ("CD" . 400) ("C" . 100) ("XC" . 90)
     ("L" . 50) ("XL" . 40) ("X" . 10)
     ("IX" . 9) ("V" . 5) ("IV" . 4) ("I" . 1)))
 
+(define digit car)
+(define value cdr)
+
 (define (to-roman number)
-  (define (aux number romans acc)
-    (cond
-      [(zero? number) acc]
-      [(<= (cdar romans) number)
-       (aux (- number (cdar romans))
-            romans
-            (string-append acc (caar romans)))]
-      [else (aux number (cdr romans) acc)]))
-  (aux number roman-values ""))
+  (string-append*
+    (for/list ([roman (in-list roman-numerals)])
+      (define-values (q r) (quotient/remainder number (value roman)))
+      (set! number r)
+      (string-append* (make-list q (digit roman))))))
