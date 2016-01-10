48a1447b8352a83849fe2cd7289cb068eb654aa0
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 19:57:45 +0100

    refactor: Add contracts, use pattern matching

--- exercism/racket/list-ops/list-ops.rkt
+++ exercism/racket/list-ops/list-ops.rkt
@@ -1,20 +1,23 @@
 #lang racket
 
-(provide my-length
-         my-reverse
-         my-map
-         my-filter
-         my-fold
-         my-append
-         my-concatenate)
+(provide
+  (contract-out
+    [my-length (-> list? natural-number/c)]
+    [my-reverse (-> list? list?)]
+    [my-map (-> procedure? list? list?)]
+    [my-filter (-> procedure? list? list?)]
+    [my-fold (-> procedure? any/c list? any/c)]
+    [my-append (-> list? list? list?)]
+    [my-concatenate (-> (listof list?) list?)]))
+
 
 (define (my-fold f acc xs)
-  (if (null? xs)
-    acc
-    (my-fold f (f (car xs) acc) (cdr xs))))
+  (match xs
+    [(cons hd tl) (my-fold f (f hd acc) tl)]
+    ['() acc]))
 
 (define (my-length xs)
-  (my-fold (λ (x acc) (add1 acc)) 0 xs))
+  (my-fold (λ (_ acc) (add1 acc)) 0 xs))
 
 (define (my-reverse xs)
   (my-fold cons '() xs))
