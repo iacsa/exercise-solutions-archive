730c70df3f5cf7f0f03b367847b09bf78e182a17
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-10 22:18:50 +0100

    refactor: Add contracts, name variables better

--- exercism/racket/difference-of-squares/difference-of-squares.rkt
+++ exercism/racket/difference-of-squares/difference-of-squares.rkt
@@ -1,17 +1,22 @@
 #lang racket
 
-(provide sum-of-squares square-of-sums difference)
+(provide
+  (contract-out
+    [sum-of-squares (-> natural-number/c natural-number/c)]
+    [square-of-sums (-> natural-number/c natural-number/c)]
+    [difference (-> natural-number/c natural-number/c)]))
 
-(define (power-sum n a b)
-  (expt (for/sum ([i (in-range (add1 n))])
-                 (expt i b))
-        a))
+
+(define (power-sum n inner outer)
+  (expt (for/sum ([i (in-range 1 (add1 n))])
+                 (expt i inner))
+        outer))
 
 (define (sum-of-squares n)
-  (power-sum n 1 2))
+  (power-sum n 2 1))
 
 (define (square-of-sums n)
-  (power-sum n 2 1))
+  (power-sum n 1 2))
 
 (define (difference n)
   (- (square-of-sums n) (sum-of-squares n)))
