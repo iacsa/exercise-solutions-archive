4d838322e64ceae80d2766d39233ac418aac9976
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-18 13:43:36 +0100

    refactor: Use list comprehension

--- exercism/haskell/point-mutations/DNA.hs
+++ exercism/haskell/point-mutations/DNA.hs
@@ -1,7 +1,4 @@
 module DNA (hammingDistance) where
 
-hammingDistance :: String -> String -> Int
-hammingDistance as bs = countTrue charsDiffer
-  where
-    charsDiffer = zipWith (/=) as bs
-    countTrue = length . filter id
+hammingDistance :: String -> String -> Integer
+hammingDistance s1 s2 = sum [1 | (c1, c2) <- zip s1 s2, c1 /= c2]
