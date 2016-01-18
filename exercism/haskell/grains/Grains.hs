8085b2dd2066632f9210a2531993efa4f5606c4f
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-01-18 13:09:58 +0100

    refactor: Make exports explicit, reuse functions

--- exercism/haskell/grains/Grains.hs
+++ exercism/haskell/grains/Grains.hs
@@ -1,7 +1,7 @@
-module Grains where
+module Grains (square, total) where
 
 square :: Integer -> Integer
-square n = 2^(n-1)
+square n = 2 ^ (n - 1)
 
 total :: Integer
-total = 2^64 - 1
+total = square 65 - 1
