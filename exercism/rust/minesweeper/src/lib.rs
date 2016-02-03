a8f1c83ffdf7d8abe032d86afc7f90c787d7a472
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-02-03 14:50:52 +0100

    refactor: replace checked vector access followed by unwrap with indexing

--- exercism/rust/minesweeper/src/lib.rs
+++ exercism/rust/minesweeper/src/lib.rs
@@ -3,14 +3,14 @@ const CHARS: &'static[char] =
 
 pub fn annotate (board: &[&str]) -> Vec<String> {
   let h = board.len();
-  let w = board.first().unwrap().len();
+  let w = board[0].len();
 
   let mut counts: Vec<usize> = vec![0; (h + 2) * (w + 2)];
   for (y, row) in board.iter().enumerate() {
     for (x, _) in row.chars().enumerate().filter(|&(_, cell)| cell == '*') {
       for dx in 0 .. 3 {
         for dy in 0 .. 3 {
-          *counts.get_mut((x + dx) * (h + 2) + y + dy).unwrap() += 1
+          counts[(x + dx) * (h + 2) + y + dy] += 1
         }
       }
     }
@@ -19,7 +19,7 @@ pub fn annotate (board: &[&str]) -> Vec<String> {
   board.iter().enumerate().map(|(y, row)|
     row.chars().enumerate().map(|(x, cell)|
       if cell == '*' { '*' }
-      else { *CHARS.get(*counts.get((x + 1) * (h + 2) + y + 1).unwrap()).unwrap() }
+      else { CHARS[counts[(x + 1) * (h + 2) + y + 1]] }
     ).collect()
   ).collect()
 }
