b556d5cf9b0aff562beab4a51cb50d9c764f3b68
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-12-21 12:55:18 +0100

    Group match cases with identical result

--- exercism/ocaml/bracket-push/bracket_push.ml
+++ exercism/ocaml/bracket-push/bracket_push.ml
@@ -5,8 +5,8 @@ let are_balanced (expr: string) : bool =
     match stack, chars with
     | ss, [] -> ss = []
     | ss, c :: cs when List.mem ['('; '['; '{'] c -> aux (c :: ss) cs
-    | '(' :: ss, ')' :: cs -> aux ss cs
-    | '[' :: ss, ']' :: cs -> aux ss cs
+    | '(' :: ss, ')' :: cs
+    | '[' :: ss, ']' :: cs
     | '{' :: ss, '}' :: cs -> aux ss cs
     | _, c :: _ when List.mem [')'; ']'; '}'] c -> false
     | ss, _ :: cs -> aux ss cs
