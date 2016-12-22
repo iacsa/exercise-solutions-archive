746535539abc198f1e4e9cc364ddb3a4f21699c7
Matthias Iacsa <matthias.iacsa@gmail.com>
2016-12-22 14:29:46 +0100

    Replaced explicit exceptions with `with_return`

--- exercism/ocaml/bracket-push/bracket_push.ml
+++ exercism/ocaml/bracket-push/bracket_push.ml
@@ -1,17 +1,13 @@
 open Core.Std
-open Core
-open Sys
 
-exception UnmatchedClosingBracket
-
-let are_balanced (expr: string) : bool =
+let are_balanced (expr: string) : bool = with_return (fun r ->
   let reduce = function
     | stack, c when List.mem ['('; '['; '{'] c -> (c :: stack)
     | '(' :: stack, ')'
     | '[' :: stack, ']'
     | '{' :: stack, '}' -> stack
-    | _, c when List.mem [')'; ']'; '}'] c -> raise UnmatchedClosingBracket
+    | _, c when List.mem [')'; ']'; '}'] c -> r.return false
     | stack, _ -> stack
   in
-    try [] = List.fold ~f:(Tuple2.curry reduce) ~init:[] (String.to_list expr)
-    with UnmatchedClosingBracket -> false
+    [] = List.fold ~f:(Tuple2.curry reduce) ~init:[] (String.to_list expr)
+)
