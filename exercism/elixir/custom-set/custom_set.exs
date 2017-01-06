0b1c967b937d98681cb608eee0166086eed17418
Matthias Iacsa <matthias.iacsa@gmail.com>
2017-01-06 14:08:39 +0100

    Completely rework according to new tests
    * Add / remove / rename methods
    * Remove behaviour 'Set' (AFAIK removed from Elixir)
    * Change interal datastructure from List to Map

--- exercism/elixir/custom-set/custom_set.exs
+++ exercism/elixir/custom-set/custom_set.exs
@@ -1,79 +1,65 @@
 defmodule CustomSet do
-  # This lets the compiler check that all Set callback functions have been implemented.
-  @behaviour Set
 
-  @type t :: %CustomSet{content: List.t}
-  defstruct content: []
+  @opaque t :: %__MODULE__ { content: Map.t }
+  defstruct content: %{}
 
-  @spec new(List.t) :: CustomSet.t
-  def new(content \\ []) do
-    %CustomSet{content: Enum.uniq(content)}
+  @spec new(Enum.t) :: t
+  def new(content) do
+    %CustomSet { content: Map.new(content, fn k -> {k, true} end) }
   end
 
-  @spec delete(CustomSet.t, any) :: CustomSet.t
-  def delete(set, item) do
-    new(List.delete(set.content, item))
+  @spec empty?(t) :: boolean
+  def empty?(set) do
+    Enum.empty?(set.content)
   end
 
-  @spec equal?(CustomSet.t, CustomSet.t) :: Bool.t
-  def equal?(set1, set2) do
-    Enum.sort(set1.content) == Enum.sort(set2.content)
+  @spec contains?(t, any) :: boolean
+  def contains?(set, item) do
+    Map.has_key?(set.content, item)
   end
 
-  @spec difference(CustomSet.t, CustomSet.t) :: CustomSet.t
-  def difference(set1, set2) do
-    Enum.reduce(set2.content, set1, &(delete(&2, &1)))
+  @spec subset?(t, t) :: boolean
+  def subset?(subset, superset) do
+    empty?(difference(subset, superset))
   end
 
-  @spec intersection(CustomSet.t, CustomSet.t) :: CustomSet.t
-  def intersection(set1, set2) do
-    difference(set1, difference(set1, set2))
-  end
-
-  @spec disjoint?(CustomSet.t, CustomSet.t) :: Bool.t
+  @spec disjoint?(t, t) :: boolean
   def disjoint?(set1, set2) do
     equal?(difference(set1, set2), set1)
   end
 
-  @spec empty(CustomSet.t) :: CustomSet.t
-  def empty(set) do
-    new
+  @spec equal?(t, t) :: boolean
+  def equal?(set1, set2) do
+    Enum.sort(set1.content) == Enum.sort(set2.content)
   end
 
-  @spec member?(CustomSet.t, any) :: Bool.t
-  def member?(set, item) do
-    Enum.member?(set.content, item)
+  @spec add(t, any) :: t
+  def add(set, item) do
+    %__MODULE__ { content: Map.put(set.content, item, true) }
   end
 
-  @spec put(CustomSet.t, any) :: CustomSet.t
-  def put(set, item) do
-    if member?(set, item), do: set, else: new([item | set.content])
+  @spec intersection(t, t) :: t
+  def intersection(set1, set2) do
+    difference(set1, difference(set1, set2))
+  end
+
+  @spec difference(t, t) :: t
+  def difference(set1, set2) do
+    %__MODULE__ { content: Map.drop(set1.content, Map.keys(set2.content)) }
+  end
+
+  @spec union(t, t) :: t
+  def union(set1, set2) do
+    %__MODULE__ { content: Map.merge(set1.content, set2.content) }
   end
 
   @spec size(CustomSet.t) :: non_neg_integer
   def size(set) do
     length(set.content)
   end
-
-  @spec subset?(CustomSet.t, CustomSet.t) :: Bool.t
-  def subset?(subset, superset) do
-    size(difference(subset, superset)) == 0
-  end
   
   @spec to_list(CustomSet.t) :: List.T
   def to_list(set) do
     Enum.sort(set.content)
-  end
-
-  @spec union(CustomSet.t, CustomSet.t) :: CustomSet.t
-  def union(set1, set2) do
-    new(set1.content ++ set2.content)
-  end
-end
-
-defimpl Inspect, for: CustomSet do
-  import Inspect.Algebra
-  def inspect(dict, opts) do
-    concat ["#<CustomSet ", to_doc(CustomSet.to_list(dict), opts), ">"]
   end
 end
