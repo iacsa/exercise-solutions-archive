8fca3ca101f08e5dcbcd0ae4a3372435bc2e7c8f
Matthias Iacsa <matthias.iacsa@gmail.com>
2019-03-04 22:52:52 +0100

    Greatly simplified solution in accordance to new tests

--- exercism/ruby/rna-transcription/rna_transcription.rb
+++ exercism/ruby/rna-transcription/rna_transcription.rb
@@ -1,15 +1,5 @@
-module BookKeeping
-  VERSION = 4
-end
-
 module Complement
-  [
-    ["dna", "GCTA"],
-    ["rna", "CGAU"]
-  ].permutation do |from, to|
-    define_singleton_method("of_#{from[0]}") do |strand|
-      return '' if not strand =~ /^[#{from[1]}]*$/
-      strand.tr(from[1], to[1])
-    end
+  def self.of_dna(strand)
+    strand.tr('GCTA', 'CGAU')
   end
 end
