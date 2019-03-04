b3dafec1fbc31911b09c11ec82028e2be61e42e1
Matthias Iacsa <matthias.iacsa@gmail.com>
2019-03-04 22:19:51 +0100

    Revise first few exercises after re-joining exercism

--- exercism/rust/hello-world/src/lib.rs
+++ exercism/rust/hello-world/src/lib.rs
@@ -1,3 +1,5 @@
-pub fn hello(name: Option<&str>) -> String {
-  format!("Hello, {}!", name.unwrap_or("World"))
+// The &'static here means the return type has a static lifetime.
+// This is a Rust feature that you don't need to worry about now.
+pub fn hello() -> &'static str {
+    "Hello, World!"
 }
